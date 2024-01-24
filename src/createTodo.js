cfe89de75f0ca0d419659bad715ecba23899d3d7
Akash <akashrangaswamy2010@gmail.com>
2024-01-23 18:20:04 -0800

    Implemented todo management functionality except type checking.

---
+++ src/createTodo.js
@@ -0,0 +1,52 @@
+const editTodos = (function () {
+  let todos = [];
+  const addProject = function (projectName) {
+    todos[projectName] = [];
+  };
+  const addTodo = function (title, description, dueDate, priority, project) {
+    todos[project].push({ title, description, dueDate, priority });
+  };
+  const getTodos = function () {
+    console.log(todos);
+    return todos;
+  };
+  const getTodo = function (project, title) {
+    console.log(todos[project].find(todo => todo.title === title));
+    return todos[project].find(todo => todo.title === title);
+  };
+  const deleteTodo = function (project, title) {
+    todos[project] = todos[project].filter(todo => todo.title !== title);
+  };
+  const editTodo = function (
+    project,
+    title,
+    newTitle = undefined,
+    newDescription = undefined,
+    newDueDate = undefined,
+    newPriority = undefined
+  ) {
+    const todo = todos[project].find(todo => todo.title === title);
+    if (newTitle !== undefined) {
+      todo.title = newTitle;
+    }
+    if (newDescription !== undefined) {
+      todo.description = newDescription;
+    }
+    if (newDueDate !== undefined) {
+      todo.dueDate = newDueDate;
+    }
+    if (newPriority !== undefined) {
+      todo.priority = newPriority;
+    }
+  };
+  return {
+    addTodo,
+    getTodos,
+    addProject,
+    getTodo,
+    deleteTodo,
+    editTodo,
+  };
+})();
+
+export { editTodos };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import './style.css';
+import { editTodos } from './createTodo.js';
+editTodos.addProject('project1');
+editTodos.addProject('project2');
+editTodos.addProject('project3');
+editTodos.addTodo(
+  'title1',
+  'description1',
+  'dueDate1',
+  'priority1',
+  'project1'
+);
+editTodos.getTodo('project1','title1');
+editTodos.getTodos();
+
